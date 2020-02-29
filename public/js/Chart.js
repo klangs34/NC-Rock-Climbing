9b67db42d311131e6e0a3de4318088c19f83dd02
kelz <kelsie.langston@yahoo.com>
2020-02-29 17:32:28 -0500

    add api routes in search page

--- public/js/Chart.js
+++ public/js/Chart.js
@@ -3936,7 +3936,7 @@ module.exports = function(Chart) {
 				// the chart initialization but after setting basic chart / controller properties that
 				// can help to figure out that the chart is not valid (e.g chart.canvas !== null);
 				// https://github.com/chartjs/Chart.js/issues/2807
-				console.error("Failed to create chart: can't acquire context from the given item");
+				//console.error("Failed to create chart: can't acquire context from the given item");
 				return;
 			}
 
