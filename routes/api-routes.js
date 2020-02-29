c4f15fcffb2373005a9929fdc404a78fb347a174
Ozymandias <raymondlee@kaffeinatenc.com>
2020-02-29 10:04:37 -0500

    Fixed UI issues

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -122,10 +122,10 @@ module.exports = app => {
       })
   });
   //get favorite routes
-  app.get("/api/get-favorite-routes/user/:user_id", function(req, res) {
+  app.get("/api/get-favorite-routes", function(req, res) {
     db.Favorites.findAll({
       where: {
-        user_id: req.params.user_id
+        user_id: req.user.id
       },
       include: [db.User, db.Routes]
     })
@@ -158,9 +158,10 @@ module.exports = app => {
   app.get("/api/routes/locate/:place", (req, res) => {
     const googleAPIKey = "AIzaSyDa0VYRLVZSiVi2MxcaF-2iORHEBcV0dHM";
     const mountainAPIKey = "200689747-d1e6e46b3dc0d8d175970060766a0430"
-    const mapURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.params.place}&inputtype=textquery&fields=geometry&key=${googleAPIKey}`;
+    const mapURL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.params.place}&inputtype=textquery&fields=geometry&key=${googleAPIKey}`;
 
     axios.get(mapURL).then(data => {
+      console.log(data);
       const latitude = data.candidates[0].geometry.location.lat;
       const longitude = data.candidates[0].geometry.location.lng;
       console.log(latitude);
