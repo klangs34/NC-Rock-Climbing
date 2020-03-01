b8e72ff137dc2e0a4da0bd4d772518a94884606a
Ozymandias <raymondlee@kaffeinatenc.com>
2020-03-01 16:12:58 -0500

    Revert "Merge pull request #32 from Jack-Storment/searchcardjs"
    This reverts commit a48fa2063c2b7a2f6b7a740ed4f95127782d50fb, reversing
    changes made to 9dcff248e9f0cd8a63ab64a6676456b7ebe9e0fb.

--- public/js/search.js
+++ public/js/search.js
@@ -1,30 +1,17 @@
-$(document).ready(function () {
-    $('#city-input').on('keypress', function (e) {
-        //e.preventDefault();
-        var place = e.target.value;
-        var keycode = (e.keyCode ? event.keyCode : event.Which);
-        if (keycode == '13') {
-            $("#search-row").empty();
-            $.get(`/api/routes/locate/${place}`)
-                .then(data => {
+$(document).ready(function() {
+    $.get('/api/routes')
+    .then(data => {
+        console.log(data)
+        var routeEl = $('<div>');
+        routeEl.append('<ul>');
+        var routeLi;
+        data.forEach((val, index) => {
+            routeLi = $('<li>');
+            routeLi.text(val.name);
+            routeLi.attr('id', index);
 
-                    const searchDiv = $("#search-row");
-                    for (i = 0; i < data.routes.length; i++) {
-                        searchDiv.append(`<div class="col s12 m3">
-                    <div class="card">
-                        <div class="card-image">
-                            <img class="responsive-img" style="height:400px" src="${data.routes[i].img}">
-                            <span class="card-title">${data.routes[i].name}</span>
-                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p>Rating: ${data.routes[i].rating} </p>
-                        </div>
-                    </div>
-                </div>`)
-                    }
-
-                })
-        }
+            routeEl.append(routeLi);
+        });
+        $('#routes').append(routeEl);
     })
 })
